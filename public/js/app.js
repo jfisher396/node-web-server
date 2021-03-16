e16ec9899f605a76cbf3f5121355d49aee830ac3
James Fisher <jfisher396@hotmail.com>
2021-03-16 12:52:31 -0700

    form created and connected to API search function

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,30 @@
 console.log("public/js/app.js is working");
 
-fetch('http://localhost:3000/weather?address=Portland').then((res) => {
-    res.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById('message-one');
+const messageTwo = document.getElementById('message-two');
+
+
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  if (!location) {
+    console.log("You must enter a valid search term.");
+  } else {
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+      res.json().then((data) => {
+         
+        if (data.err) {
+          messageOne.textContent = data.err;
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
         }
-        
-    })
-})
+      });
+    });
+  }
+});
